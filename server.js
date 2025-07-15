e03db7d1dd8ad16a3c1f76b1c2948aa32b88d73a
ibtihel <ibtiheljenzri2@gmail.com>
2025-07-15 23:00:49 +0200

    hello

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = require('./src/app');
 const dbConfig = require('./src/config/db');
+const uploadRoute = require('./src/routes/profile');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+
+app.use('/uploads', express.static('uploads')); 
+
+
+
 
 // Connect to MongoDB
 dbConfig();
