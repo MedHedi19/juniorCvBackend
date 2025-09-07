e9d8c4c7f044c38f9ea09ea3070ad8f7ad355024
MedHedi19 <bensalahm99@gmail.com>
2025-09-07 23:43:30 +0100

    a

--- src/models/user.js
+++ src/models/user.js
@@ -34,13 +34,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: [8, 'Password must be at least 8 characters long'],
-        validate: {
-            validator: function(v) {
-                // At least 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
-                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v);
-            },
-            message: 'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character'
-        }
     },
     profilePhoto: {
         type: String,
